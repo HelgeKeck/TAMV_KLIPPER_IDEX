22d71e2c9c1e6e6ee64e3810b998d48dddaa32f4
HelgeKeck <helgekeck@hotmail.com>
2023-05-01 19:33:52 +0200

    Update tamv.py

--- klippy_extra/tamv.py
+++ klippy_extra/tamv.py
@@ -4,69 +4,67 @@ class TAMV:
         self.config = config
         self.printer = self.config.get_printer()
         self.gcode = self.printer.lookup_object('gcode')
-
-        self.offset = None                  # Offset of the nozzle in the format X, Y, Z
-        self.tool_current = "-2"            # -2 Unknown tool locked, -1 No tool locked, 0 and up are tools.
-
-        self.gcode.register_command('TAMV_SET_TOOL', self.cmd_TAMV_SET_TOOL, desc=("TAMV_SET_TOOL"))
-        self.gcode.register_command('TAMV_SET_TOOL_OFFSET', self.cmd_SET_TOOL_OFFSET, desc=("SET_TOOL_OFFSET"))
+        self.offset = self.load_offset()
+        self.gcode.register_command('TAMV_SET_OFFSET', self.cmd_TAMV_SET_OFFSET, desc=("TAMV_SET_OFFSET"))
+        self.gcode.register_command('TAMV_SAVE_OFFSET', self.cmd_TAMV_SAVE_OFFSET, desc=("TAMV_SAVE_OFFSET"))
         self.printer.register_event_handler("klippy:connect", self.handle_connect)
 
     def handle_connect(self):
         self.toolhead = self.printer.lookup_object('toolhead')
 
-    def cmd_TAMV_SET_TOOL(self, param):
-        tool = param.get_int('TOOL', -1) 
-        self.tool_current = str(tool)
+    def cmd_TAMV_SET_OFFSET(self, gcmd):
+        tool = gcmd.get_int('TOOL', -1)
+        self.respond("TAMV_SET_OFFSET " + str(gcmd))
+        if tool == 1:
+            x_adjust = gcmd.get_float('X', None)
+            y_adjust = gcmd.get_float('Y', None)
+            #z_adjust = gcmd.get_float('Z', None)
+            self.offset = [x_adjust, y_adjust, self.offset[2]]
 
-        self.respond(str(self.toolhead['name']))
-
-        self.save_variable("tool_current", tool)
-        self.respond(self.load_variable("tool_current"))
-        # tool_current = self.load_variable("tool_current")
-        # self.save_variable("tool_current", tool)
-
-    def cmd_SET_TOOL_OFFSET(self, gcmd):
-        tool_id = gcmd.get_int('TOOL', self.tool_current, minval=0)
-        x_pos = gcmd.get_float('X', None)
-        x_adjust = gcmd.get_float('X_ADJUST', None)
-        y_pos = gcmd.get_float('Y', None)
-        y_adjust = gcmd.get_float('Y_ADJUST', None)
-        z_pos = gcmd.get_float('Z', None)
-        z_adjust = gcmd.get_float('Z_ADJUST', None)
-
-        if tool_id < 0:
-            self.gcode.respond_info("cmd_SET_TOOL_TEMPERATURE: Tool " + str(tool_id) + " is not valid.")
-            return None
-
-        tool = self.printer.lookup_object("tool " + str(tool_id))
-        set_offset_cmd = {}
-
-        if x_pos is not None:
-            set_offset_cmd["x_pos"] = x_pos
-        elif x_adjust is not None:
-            set_offset_cmd["x_adjust"] = x_adjust
-        if y_pos is not None:
-            set_offset_cmd["y_pos"] = y_pos
-        elif y_adjust is not None:
-            set_offset_cmd["y_adjust"] = y_adjust
-        if z_pos is not None:
-            set_offset_cmd["z_pos"] = z_pos
-        elif z_adjust is not None:
-            set_offset_cmd["z_adjust"] = z_adjust
-        if len(set_offset_cmd) > 0:
-            tool.set_offset(**set_offset_cmd)
+    def cmd_TAMV_SAVE_OFFSET(self, gcmd):
+        self.save_offset(self.offset)
+        self.respond("Offset saved")
 
     def get_status(self, eventtime=None):
         status = {
-            "tool_current": self.tool_current
+            "current_tool": self.get_current_tool(),
+            "offset": self.offset
         }
         return status
 
+    def save_offset(self, offset):
+        self.save_variable("xoffset", offset[0])
+        self.save_variable("yoffset", offset[1])
+        #self.save_variable("zoffset", offset[2])
+
+    def load_offset(self):
+        xoffset = self.load_variable("xoffset")
+        yoffset = self.load_variable("yoffset")
+        zoffset = self.load_variable("zoffset")
+        return [xoffset, yoffset, zoffset]
+
+    def get_current_tool(self):
+        if not self.is_homed():
+            return -1
+        if self.toolhead.extruder.get_name() == 'extruder':
+            return 0
+        if self.toolhead.extruder.get_name() == 'extruder1':
+            return 1
+        return -1
+
+    def is_homed(self):
+        curtime = self.printer.get_reactor().monotonic()
+        homed = self.toolhead.get_status(curtime)['homed_axes'].lower()
+        if all(axis in homed for axis in ['x','y','z']):
+            return True
+        return False
+
+    # current_tool = self.load_variable("current_tool")
     def load_variable(self, name):
         save_variables = self.printer.lookup_object('save_variables')
         return save_variables.allVariables[name]
 
+    # self.save_variable("current_tool", tool)
     def save_variable(self, name, value):
         save_variables = self.printer.lookup_object('save_variables')
         save_variables.cmd_SAVE_VARIABLE(self.gcode.create_gcode_command(
